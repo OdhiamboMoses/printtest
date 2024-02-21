655472e6ace173896dfe85964b9e433b9ab46f4b
odhiambomoses <odhiambomoses08@gmail.com>
2024-02-21 12:06:47 +0300

    print test

--- printtest.py
+++ printtest.py
@@ -10,4 +10,15 @@ print("The last letter in name variable is "+" "+name[-1])
 index=len(text)-1
 print(text[index])
 print("The index variable type is "+" "+str(type(index)))
+height=5
+base=3
+heightsquared=height**height
+basesquared=base**base
+csquared=heightsquared+basesquared
+c=math.sqrt(csquared)
+print("The value of c in the triangle is "+str(c))
+fruits=['Banana','Apple','Grapes','Oranges']
+print(fruits)
+print("Fruit on index 3 is "+fruits[3])
+print("Fruit on index 0 is "+fruits[0])
 
