3d82d97c3a8e47f860ccac989e0b9c348982a239
odhiambomoses <odhiambomoses08@gmail.com>
2024-02-21 11:46:13 +0300

    hello world

--- printtest.py
+++ printtest.py
@@ -1,6 +1,13 @@
+import math
 text="Hello World"
 name="Moses Odhiambo"
 print(len(text))
 print(text)
 print(type(text))
 print(text+" "+name)
+print("The letter in index 9 is "+" "+text[9])
+print("The last letter in name variable is "+" "+name[-1])
+index=len(text)-1
+print(text[index])
+print("The index variable type is "+" "+str(type(index)))
+
